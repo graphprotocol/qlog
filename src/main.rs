a4469bebe0bd9dadf931d38f39b1cbf82a184915
David Lutterkort <lutter@watzmann.net>
2020-06-16 12:56:55 -0700

    qlog: Sample queries uniformly, and only distinct queries
    The sampling process considers only unique `(query, variables)` entries in
    the input stream for each subgraph; from that stream of unique queries, we
    uniformly take up the given number of samples for each subgraph.

--- src/main.rs
+++ src/main.rs
@@ -102,10 +102,63 @@ impl Sample {
     }
 }
 
+/// A collection of query samples; we use one of these for each subgraph.
+struct SampleDomain {
+    /// The total number of unique queries we have seen
+    seen_count: usize,
+    /// The hashes of unique `(query, variables)` combinations
+    seen: HashSet<u64>,
+    /// Up to `Sampler.size` distinct samples
+    samples: Vec<Sample>,
+}
+
+impl Default for SampleDomain {
+    fn default() -> Self {
+        SampleDomain {
+            seen_count: 0,
+            seen: HashSet::default(),
+            samples: Vec::default(),
+        }
+    }
+}
+
+impl SampleDomain {
+    /// If we have not seen `(query, variables)` before, add them to our samples
+    /// so that in the end the probability that any unique query is in our
+    /// final sample is `size / N` where `N` is the number of distinct queries
+    fn sample(&mut self, size: usize, rng: &mut SmallRng, query: &str, variables: &Option<&str>) {
+        let hash = {
+            let mut hasher = DefaultHasher::new();
+            (query, variables).hash(&mut hasher);
+            hasher.finish()
+        };
+
+        // We sample distinct queries
+        if !self.seen.contains(&hash) {
+            // Sample uniformly, i.e. if there are N distinct queries for a
+            // subgraph in the file we are processing, the probabilty that any
+            // one query winds up in the sample is `size/N`
+            if self.seen_count < size {
+                self.samples.push(Sample::new(query, variables));
+            } else {
+                let k = rng.gen_range(0, self.seen_count + 1);
+                if k < size {
+                    let samples = Sample::new(query, variables);
+                    if let Some(entry) = self.samples.get_mut(k) {
+                        *entry = samples;
+                    }
+                }
+            }
+            self.seen_count += 1;
+            self.seen.insert(hash);
+        }
+    }
+}
+
 struct Sampler {
     size: usize,
     rng: SmallRng,
-    samples: BTreeMap<String, Vec<Sample>>,
+    samples: BTreeMap<String, SampleDomain>,
     subgraphs: HashSet<String>,
 }
 
@@ -128,23 +181,14 @@ impl Sampler {
             return;
         }
 
-        let samples = {
+        let domain = {
             match self.samples.get_mut(subgraph) {
                 Some(samples) => samples,
                 None => self.samples.entry(subgraph.to_owned()).or_default(),
             }
         };
-        let sample = Sample::new(query, variables);
-        if samples.len() < self.size {
-            samples.push(sample);
-        } else {
-            let k = self.rng.gen_range(0, self.size);
-            if let Some(entry) = samples.get_mut(k) {
-                *entry = sample;
-            } else {
-                samples.push(sample);
-            }
-        }
+
+        domain.sample(self.size, &mut self.rng, query, variables);
     }
 
     fn write(&self, mut out: BufWriter<File>) -> Result<(), std::io::Error> {
@@ -155,8 +199,8 @@ impl Sampler {
             #[serde(skip_serializing_if = "Option::is_none")]
             variables: &'a Option<String>,
         }
-        for (subgraph, samples) in &self.samples {
-            for sample in samples {
+        for (subgraph, domain) in &self.samples {
+            for sample in &domain.samples {
                 let v = SampleOutput {
                     subgraph,
                     query: &sample.query,
